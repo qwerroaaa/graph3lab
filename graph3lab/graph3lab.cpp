6240989d19f0119f078568ed76a5b777538da88c
Serbushko Andrey <serbushko.qwe@gmail.com>
2023-06-06 19:54:48 +0700

    Add project files.

---
+++ graph3lab/graph3lab.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+struct Point
+{
+    double x, y;
+};
+
+void buildCompleteGraph(const Point* Point, int N, double** graph)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i == j)
+            {
+                graph[i][j] = 0;
+            }
+            else
+            {
+                graph[i][j] = sqrt(pow(Point[i].x - Point[j].x, 2));
+            }
+        }
+    }
+}
+
+int main()
+{
+    const int N = 15, K = 5;
+    Point points[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> points[i].x >> points[i].x;
+    }
+}
