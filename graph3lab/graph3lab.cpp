13ac6974895e1cc848865fb5a810341f3d7242f9
Serbushko Andrey <serbushko.qwe@gmail.com>
2023-06-08 08:20:46 +0700

    fisrt

--- graph3lab/graph3lab.cpp
+++ graph3lab/graph3lab.cpp
@@ -2,47 +2,72 @@
 #include <math.h>
 #include <cstdlib>
 #include <algorithm>
-
+#include <vector>
 #define INF DBL_MAX
 using namespace std;
 
 struct Point {
     double x, y;
+    Point(double _x, double _y) : x(_x), y(_y) {}
     Point()
     {
         x = 1 + rand() % 100;
         y = 1 + rand() % 100;
     }
-    double distance(const Point p1, const Point p2)
+    static double distance(const Point p1, const Point p2)
     {
         return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
     }
 };
 
+struct Edge
+{
+    int u, v;
+    double weight;
+    Edge(int _u, int _v, double _weight) : u(_u), v(_v), weight(_weight) {}
+};
+
 class MGraph
 {
-    double** weigths;
-    int vernum; 
-    bool oriented;
+    double** weights;
+    int vernum; //вершины
+    vector<Point> vertexes;
 public:
     MGraph(int _vernum): vernum(_vernum)
     {
-        //some code
-    }
-
-    void buildCompleteGraph()
-    {
-        double distance;
         for (int i = 0; i < vernum; i++)
         {
-            for (int j = 0; i < vernum; j++)
-            {
-                weigths[i][j] = distance;
+            vertexes.push_back(Point());
+        }
+
+        weights = new double* [vernum];
+        for (int i = 0; i < vernum; i++) {
+            weights[i] = new double[vernum];
+            for (int j = 0; j < vernum; j++) {
+                weights[i][j] = INF; // Инициализируем все веса нулями
             }
         }
     }
 
-    void get_span_tree() {
+    void buildCompleteGraph()
+    {
+        for (int i = 0; i < vernum; i++)
+        {
+            for (int j = 0; i < vernum; j++)
+            {
+                weights[i][j] = Point::distance(vertexes[i], vertexes[j]);
+            }
+        }
+    }
+
+    void add_edge(int u, int v)
+    {
+        weights[u][v] = Point::distance(vertexes[u], vertexes[v]);
+    }
+
+    //алгоритм примо
+    vector<Edge> getSpanTree() {
+        vector<Edge> EdgesOfTree;
         double wmin;
         int i, j, vm, * B = new int[vernum];
         B[0] = -1;
@@ -50,16 +75,19 @@ public:
         for (i = 1; i < vernum; i++) {
             wmin = INF; vm = 0;
             for (j = 1; j < vernum; j++)
-                if (B[j] != -1 && wmin > weigths[j][B[j]])
+                if (B[j] != -1 && wmin > weights[j][B[j]])
                 {
-                    vm = j; wmin = weigths[j][B[j]];
+                    vm = j; wmin = weights[j][B[j]];
                 }
-            if (!vm) return;
-            add_edge(vm, B[vm]); B[vm] = -1;
+            if (!vm) return EdgesOfTree;
+            add_edge(vm, B[vm]);
+            EdgesOfTree.push_back(Edge(vm, B[vm], weights[vm][B[vm]]));
+            B[vm] = -1;
             for (j = 1; j < vernum; j++)
-                if (B[j] != -1 && weigths[j][B[j]] > weigths[j][vm])
+                if (B[j] != -1 && weights[j][B[j]] > weights[j][vm])
                     B[j] = vm;
         }
+        return EdgesOfTree;
     }
 };
 
@@ -67,8 +95,11 @@ int main()
 {
     int N = 15, K = 5;
     MGraph graph(N);
+    graph.buildCompleteGraph();
 
+    vector<Edge> OstovGraph = graph.getSpanTree(); // выделение минимального остова
 
+    sort(OstovGraph.begin(), OstovGraph.end()); //отсортировать ребра остова по возрастанию весов.
 
     bool** adj = new bool* [N];
     for (int i = 0; i < N; i++)
@@ -79,6 +110,4 @@ int main()
             adj[i][j] = 0;
         }
     }
-
-    cout << "test";
 }
